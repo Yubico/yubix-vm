c9f1ec87864ed0be0d77441e892a781cb94ddf50
Dain Nilsson <dain@yubico.com>
2013-02-28 12:20:45 +0100

    Initial import.

---
+++ build-vm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+echo $DIR
+
+exit 0
+
+HYPERVISOR="vmw6"
+DIST="ubuntu"
+
+VMBUILDER_ARGS="--suite precise --arch i386 --flavour virtual \
+--mem 300 --tmpfs - --hostname yubi-x --user yubikey --pass yubico \
+--ppa yubico/stable --addpkg unattended-upgrades --addpkg ubuntu-desktop \
+--addpkg yubi-x"
+
+vmbuilder $HYPERVISOR $DIST $VMBUILDER_ARGS
+
+if [ $HYPERVISOR == "vmw6" ]; then
+	#Fix path of the virtual hdd.
+	sed -i s,$DIR/$DIST-$HYPERVISOR/,,g $DIST-$HYPERVISOR/yubi-x.vmx
+fi
