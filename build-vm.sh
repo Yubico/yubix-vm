11351d6ab9d0fa27c399ac88340ee0afd94fd711
Dain Nilsson <dain@yubico.com>
2013-03-08 12:15:03 +0100

    Create image in output/

--- build-vm.sh
+++ build-vm.sh
@@ -6,18 +6,20 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 HYPERVISOR="vmw6"
 DIST="ubuntu"
+DEST="output"
 
 VMBUILDER_ARGS="--suite precise --arch i386 --flavour virtual \
 --mem 512 --tmpfs - --hostname yubi-x --user yubikey --pass yubico \
 --copy $DIR/copy --exec $DIR/exec.sh --firstboot $DIR/firstboot.sh \
---ppa yubico/stable --addpkg unattended-upgrades --addpkg acpid \
---addpkg yubi-x --addpkg pwgen --addpkg ssh"
+--dest $DEST --verbose --ppa yubico/stable \
+--addpkg unattended-upgrades --addpkg acpid --addpkg yubi-x \
+--addpkg pwgen --addpkg ssh"
 
-vmbuilder $HYPERVISOR $DIST $VMBUILDER_ARGS $@
+vmbuilder $HYPERVISOR $DIST $VMBUILDER_ARGS "$@"
 
 if [ $HYPERVISOR == "vmw6" ]; then
 	#Fix path of the virtual hdd.
-	sed -i s,$DIR/$DIST-$HYPERVISOR/,,g $DIST-$HYPERVISOR/yubi-x.vmx
+	sed -i s,$DIR/$DEST/,,g $DEST/yubi-x.vmx
 fi
 
 echo "Completed successfully!"
